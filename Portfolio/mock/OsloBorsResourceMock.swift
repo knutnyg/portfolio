22b168f16eeffd9df1aaf27ebd6bb41d19b72a4c
Knut Nygaard <knutnyg@gmail.com>
2016-03-13 11:44:06 +0100

    Add mock for oslo resource

--- Portfolio/mock/OsloBorsResourceMock.swift
+++ Portfolio/mock/OsloBorsResourceMock.swift
@@ -8,5 +8,34 @@ class OsloBorsResourceMock : OsloBorsResource {
         return Future(value: AllStockInfo())
     }
 
+    override func getCurrentValueForStock(store: Store, stock: Stock) -> Future<Stock, NSError> {
+        let s:Stock = Stock(ticker: stock.ticker)
+        s.currentValueTimestamp = NSDate()
+        s.currentValue = Double(arc4random_uniform(300))
 
+        return Future(value: s)
+    }
+
+    override func getHistoryForStock(store: Store, stock: Stock) -> Future<Stock, NSError> {
+
+        let instances = [
+                StockPriceInstance(date: NSDate(), price: Double(arc4random_uniform(300) + 1)),
+                StockPriceInstance(date: NSDate(timeInterval: -86400 * 1, sinceDate: NSDate()), price: Double(arc4random_uniform(300) + 1)),
+                StockPriceInstance(date: NSDate(timeInterval: -86400 * 2, sinceDate: NSDate()), price: Double(arc4random_uniform(300) + 1)),
+                StockPriceInstance(date: NSDate(timeInterval: -86400 * 3, sinceDate: NSDate()), price: Double(arc4random_uniform(300) + 1)),
+                StockPriceInstance(date: NSDate(timeInterval: -86400 * 4, sinceDate: NSDate()), price: Double(arc4random_uniform(300) + 1)),
+                StockPriceInstance(date: NSDate(timeInterval: -86400 * 5, sinceDate: NSDate()), price: Double(arc4random_uniform(300) + 1)),
+                StockPriceInstance(date: NSDate(timeInterval: -86400 * 6, sinceDate: NSDate()), price: Double(arc4random_uniform(300) + 1)),
+                StockPriceInstance(date: NSDate(timeInterval: -86400 * 7, sinceDate: NSDate()), price: Double(arc4random_uniform(300) + 1)),
+                StockPriceInstance(date: NSDate(timeInterval: -86400 * 8, sinceDate: NSDate()), price: Double(arc4random_uniform(300) + 1)),
+                StockPriceInstance(date: NSDate(timeInterval: -86400 * 9, sinceDate: NSDate()), price: Double(arc4random_uniform(300) + 1)),
+                StockPriceInstance(date: NSDate(timeInterval: -86400 * 10, sinceDate: NSDate()), price: Double(arc4random_uniform(300) + 1))
+        ]
+
+        let s:Stock = Stock(ticker: stock.ticker)
+        s.history = StockHistory(history: instances)
+        s.historyTimestamp = NSDate()
+
+        return Future(value: s)
+    }
 }
--- PortfolioTests/rest/OsloBorsResouceTests.swift
+++ PortfolioTests/rest/OsloBorsResouceTests.swift
@@ -30,20 +30,64 @@ class OsloBorsResouceTests : XCTestCase {
     }
 
     func testGetHistories(){
+
+        if skipIntegration {
+            return
+        }
+
         let expectation = expectationWithDescription("promise")
 
         let store = Store()
         OsloBorsResource().updateStockHistories(store, stocks: [Stock(ticker: "NAS.OSE")])
         .onSuccess {
             stocks in
-
-            print("testing")
             XCTAssert(stocks[0].ticker == "NAS.OSE")
-            print("testing1")
             XCTAssert(stocks[0].history != nil)
-            print("testing2")
             XCTAssert(stocks[0].historyTimestamp != nil)
-            print("testing3")
+            expectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(10, handler: {
+            error in
+            XCTAssertNil(error, "Error")
+        })
+    }
+
+    func testGetHistoriesMock(){
+
+        let expectation = expectationWithDescription("promise")
+
+        let store = Store()
+        OsloBorsResourceMock().updateStockHistories(store, stocks: [Stock(ticker: "NAS.OSE")])
+        .onSuccess {
+            stocks in
+            XCTAssert(stocks[0].ticker == "NAS.OSE")
+            XCTAssert(stocks[0].history != nil)
+            XCTAssert(stocks[0].historyTimestamp != nil)
+            expectation.fulfill()
+        }
+
+        waitForExpectationsWithTimeout(10, handler: {
+            error in
+            XCTAssertNil(error, "Error")
+        })
+    }
+
+    func testGetCurrentValuesMock(){
+
+        let expectation = expectationWithDescription("promise")
+
+        let store = Store()
+        OsloBorsResourceMock().updateStocksCurrentValue(store, stocks: [Stock(ticker: "NAS.OSE"), Stock(ticker: "NOD.OSE")])
+        .onSuccess {
+            (stocks:[Stock]) in
+            XCTAssert(stocks[0].ticker == "NAS.OSE")
+            XCTAssert(stocks[0].currentValue > 0 && stocks[0].currentValue < 301)
+            XCTAssert(stocks[0].currentValueTimestamp!.isInSameDayAs(date: NSDate()))
+
+            XCTAssert(stocks[1].ticker == "NOD.OSE")
+            XCTAssert(stocks[1].currentValue > 0 && stocks[0].currentValue < 301)
+            XCTAssert(stocks[1].currentValueTimestamp!.isInSameDayAs(date: NSDate()))
             expectation.fulfill()
         }
 
